af1abb6064f9c1ab0e3d4544fc16733bf0720fc5
Jim Pick <jim@backblend.com>
2019-07-04 20:08:07 -0700

    First attempt at a client

---
+++ go.mod
@@ -0,0 +1,12 @@
+module github.com/jimpick/go-simple-testrig-client
+
+go 1.12
+
+require (
+	github.com/libp2p/go-libp2p v0.2.0
+	github.com/libp2p/go-libp2p-core v0.0.6
+	github.com/libp2p/go-libp2p-gostream v0.1.2
+	github.com/libp2p/go-libp2p-http v0.1.3
+	github.com/libp2p/go-libp2p-peerstore v0.1.1
+	github.com/multiformats/go-multiaddr v0.0.4
+)
---
+++ main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+
+	"github.com/libp2p/go-libp2p"
+	"github.com/libp2p/go-libp2p-core/host"
+	peerstore "github.com/libp2p/go-libp2p-peerstore"
+	"github.com/libp2p/go-libp2p-http"
+	multiaddr "github.com/multiformats/go-multiaddr"
+)
+
+// newHost illustrates how to build a libp2p host with secio using
+// a randomly generated key-pair
+func newHost(listen multiaddr.Multiaddr) host.Host {
+	h, err := libp2p.New(
+		context.Background(),
+		libp2p.ListenAddrs(listen),
+	)
+	if err != nil {
+		panic(err)
+	}
+	// print the node's listening addresses
+	fmt.Println("Listen addresses:", h.Addrs())
+	return h
+}
+
+func main() {
+	fmt.Println("Jim1", len(os.Args))
+	if len(os.Args) < 2 {
+		panic("Need multiaddr")
+	}
+	fmt.Println("Jim2", os.Args[1])
+	addr, err := multiaddr.NewMultiaddr(os.Args[1])
+	if err != nil {
+		panic(err)
+	}
+	peer, err := peerstore.InfoFromP2pAddr(addr)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Connecting to:", os.Args[1])
+	fmt.Println("Peer:", peer.ID)
+
+	m2, _ := multiaddr.NewMultiaddr("/ip4/127.0.0.1/tcp/10001")
+	clientHost := newHost(m2)
+	defer clientHost.Close()
+
+	clientHost.Peerstore().AddAddrs(peer.ID, peer.Addrs, peerstore.PermanentAddrTTL)
+
+	// print the node's PeerInfo in multiaddr format
+	peerInfo := &peerstore.PeerInfo{
+		ID:    clientHost.ID(),
+		Addrs: clientHost.Addrs(),
+	}
+	addrs, err := peerstore.InfoToP2pAddrs(peerInfo)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("libp2p node address:", addrs[0])
+
+	tr := &http.Transport{}
+	tr.RegisterProtocol("libp2p", p2phttp.NewTransport(clientHost))
+	client := &http.Client{Transport: tr}
+	fmt.Println("Jim3:", "libp2p://" + peer.ID.String())
+	res, err := client.Get("libp2p://" + peer.ID.String())
+	if err != nil {
+		panic(err)
+	}
+	text, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("res text: %s\n", text)
+}
